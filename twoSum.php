7ab2399d570836f70a396b3f6c33e171a9d56fcc
zl-sky <42759476+zl-phper@users.noreply.github.com>
2021-04-07 13:45:34 +0800

    两数之和 hash 方式

---
+++ twoSum.php
@@ -0,0 +1,23 @@
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $hash = [];
+        $n = count($nums);
+        for ($i = 0; $i < $n; ++$i) {
+            $diff = $target - $nums[$i];
+            if (isset($hash[$diff])) return [$i, $hash[$diff]];
+            $hash[$nums[$i]] = $i;
+        }
+        return [];
+    }
+}
